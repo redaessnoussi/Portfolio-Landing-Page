d0c532a05da3003645bfaa519a18da0e1d34df93
wolf21995 <71893153+wolf21995@users.noreply.github.com>
2021-03-29 22:42:04 +0100

    added starter pack

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+$(document).ready(function() {
+  $('body').autoPadding({
+    source: $('.js-header'),
+  });
+    //removeIf(production)
+    console.log("document ready");
+    //endRemoveIf(production)
+});
